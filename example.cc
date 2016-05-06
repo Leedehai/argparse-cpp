8f5634272056655105432fe6013f74f5746d3751
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2016-05-06 16:01:05 +0900

    add example code (example.cc) and update README

--- example.cc
+++ example.cc
@@ -1,12 +1,3 @@
-argparse-cpp
-============================
-
-Argument parser for CLI in C++. This is inspired by ArgumentParser in Python.
-
-Example
-----------------
-
-```cpp
 #include <iostream>
 
 #include "./argparse.hpp"
@@ -17,24 +8,11 @@ int main(int argc, char *argv[]) {
     .metavar("FILE").help("Configuration file");
   psr.add_argument("-v").name("--verbose").action("store_true")
     .help("verbose output");
-
+  
   argparse::Values val = psr.parse_args(argc, argv);
-  std::cout
-    << "config:  " << val["config"] << std::endl
+  std::cout 
+    << "config:  " << val["config"] << std::endl 
     << "verbose: " << val.is_true("verbose") << std::endl;
 
   return 0;
 }
-```
-
-
-Author
------------------
-
-Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
-
-License
------------------
-
-The BSD 3-Clause License. [see License](LICENSE.md).
-
